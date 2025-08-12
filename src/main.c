7131b80f68123366bfda5cbd6b0929d9c23d3cbe
DiegoMX <diego.perez@modular-mx.com>
2025-08-12 18:41:21 +0000

    added empty serial task

--- src/main.c
+++ src/main.c
@@ -6,12 +6,14 @@
  */
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 
 /**
  * @defgroup main Main Application
  * @brief Main application entry point.
  @{ */
 #define HB_TASK_PRIORITY 5  /*!< Priority of the heartbeat task. */
+#define SERIAL_TASK_PRIORITY 6 /*!< Priority of the serial task. */
 /**
  @} */
 
@@ -19,23 +21,37 @@
  * @brief Heartbeat task stack area made of 1024 bytes
  */
 K_THREAD_STACK_DEFINE( hb_task_stack, 1024 );
+/**
+ * @brief Serial task stack area made of 1024 bytes
+ */
+K_THREAD_STACK_DEFINE( serial_task_stack, 1024 );
 
 /**
  * @brief Heartbeat task ID.
  */
 static k_tid_t  hb_task_id;
+/**
+ * @brief Serial task ID.
+ */
+static k_tid_t  serial_task_id;
 
 /**
  * @brief Heartbeat task control block.
  */
 static struct k_thread hb_task_tcb;
+/**
+ * @brief Serial task control block.
+ */
+static struct k_thread serial_task_tcb;
 
 /**
  * @brief Heartbeat LED GPIO descriptor.
  */
 const static struct gpio_dt_spec hb_led = GPIO_DT_SPEC_GET( DT_ALIAS( heartbeat ), gpios );
 
+
 static void hb_task( void *p1, void *p2, void *p3 );
+static void serial_task( void *p1, void *p2, void *p3 );
 
 /**
  * @brief Main function of the application.
@@ -49,6 +65,10 @@ int main( void )
     /* Register the heartbeat thread and run immediately */
 	hb_task_id = k_thread_create( &hb_task_tcb, hb_task_stack, K_THREAD_STACK_SIZEOF( hb_task_stack ),
                             	hb_task, NULL, NULL, NULL, HB_TASK_PRIORITY, 0, K_NO_WAIT );
+
+    /* Register the serial thread and run immediately */
+    serial_task_id = k_thread_create( &serial_task_tcb, serial_task_stack, K_THREAD_STACK_SIZEOF( serial_task_stack ),
+                            	serial_task, NULL, NULL, NULL, SERIAL_TASK_PRIORITY, 0, K_NO_WAIT );
 
     return 0; // This line will never be reached
 }
@@ -72,5 +92,23 @@ static void hb_task( void *p1, void *p2, void *p3 )
         /* Toggle an LED or perform a heartbeat action */
         gpio_pin_toggle_dt( &hb_led );
         k_msleep( 1000 ); // Sleep for 1 second
+    }
+}
+
+/**
+ * @brief Serial task function.
+ *
+ * This function runs in a loop, performing serial communication tasks.
+ *
+ * @param p1 Pointer to the first parameter (not used).
+ * @param p2 Pointer to the second parameter (not used).
+ * @param p3 Pointer to the third parameter (not used).
+ */
+static void serial_task( void *p1, void *p2, void *p3 )
+{
+    /* This task could handle serial communication, e.g., reading from UART */
+    while ( true ) {
+        /* Perform serial communication tasks */
+        k_msleep( 500 ); // Sleep for 0.5 seconds
     }
 }
